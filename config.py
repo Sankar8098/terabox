983551ab4e7218e4bfedcb354c69b6fcf7eb7dfb
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-27 13:58:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
         load_dotenv()
         self.API_ID = os.getenv("24444928")
         self.API_HASH = os.getenv("0a278a515cd13ec8802b7dabed73dede")
-        self.BOT_TOKEN = os.getenv("7029092370:AAEhUE-EibLL1Akv26od7PCv0rqL1mXk_m0")
+        self.BOT_TOKEN = os.getenv("7029092370:AAFQU0Ve0zDlw_tKqx0sDC-mOgVRdBT1Ss0")
         self.DB_NAME = os.getenv("DB_NAME", "terabox_links.db")
         self.MAX_CONCURRENT_DOWNLOADS = int(os.getenv("MAX_CONCURRENT_DOWNLOADS", 5))
         self.BOT_USERNAME = os.getenv("Terabox_download_Sk_bot")
